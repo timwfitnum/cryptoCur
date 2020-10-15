6c1494dfb3f2a7fdc71e963a55dd8c748fc82992
timwfitnum <72702393+timwfitnum@users.noreply.github.com>
2020-10-15 12:09:42 +0200

    Update coincap_v1.py

--- src/coincap_v1.py
+++ src/coincap_v1.py
@@ -13,7 +13,7 @@ quotes_url= 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?s
 conversion = '&convert=' + convert
 headers = {
   'Accepts': 'application/json',
-  'X-CMC_PRO_API_KEY': '5ba046a8-ce16-4013-8b5c-7370e96a4e1a',
+  'X-CMC_PRO_API_KEY': 'YOUR_API_KEY',
 }
 
 session = Session()
